0342c1cc76016e758946d7db3376874f2271ee6f
sakurahuang <sakurahuang@tencent.com>
2023-03-13 17:53:17 +0800

    字符串操作

--- hello/Hello/Hello/Program.cs
+++ hello/Hello/Hello/Program.cs
@@ -19,9 +19,25 @@ namespace Hello
             //Console.WriteLine(str);
             //Console.Read();
 
-            ConsoleKeyInfo a = Console.ReadKey();
-            Console.WriteLine(a.Key);
-            Console.Clear();
+            //ConsoleKeyInfo a = Console.ReadKey();
+            //Console.WriteLine(a.Key);
+            //Console.Clear();
+            //Console.Read();
+            //Console.WriteLine("My name is {0}.I am {1} years old", "hty", 18);
+            string name = "hty";
+            int age = 18;
+            //string message = "My name is {0}. I am {1} years old.";
+            //string output = string.Format(message, name, age);
+            //Console.WriteLine(output);
+            //string message = $"My name is {name}. I am {age} years old.";
+            //Console.WriteLine(message);
+            string str = @"test next line";
+
+
+            //string hello = "hello";
+            //string str = hello.ToUpper();
+            Console.WriteLine(str);
+
             Console.Read();
         }
     }

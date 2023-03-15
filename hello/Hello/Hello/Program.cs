5aa3007d9c91ec89e8dac763f4b88bda4d58b741
sakurahuang <sakurahuang@tencent.com>
2023-03-15 15:37:43 +0800

    数组方法中的index与ranage

--- hello/Hello/Hello/Program.cs
+++ hello/Hello/Hello/Program.cs
@@ -8,6 +8,24 @@ namespace Hello
         {
             private int x;
             private int y;
+            public string[] gamma = new string[]
+        {
+            "Test", // 0  ^3
+            "csharp",// 1 ^2
+            "Array" // 2 ^1
+        };
+
+            public string this[int index]
+            {
+                get
+                {
+                    return gamma[index];
+                }
+                set
+                {
+                    gamma[index] = value;
+                }
+            }
 
             public Point(int x, int y)
             {
@@ -57,18 +75,38 @@ namespace Hello
             
         }
 
-        
-            
+        static string[] words = new string[]
+        {
+            "Test", // 0  ^3
+            "csharp",// 1 ^2
+            "Array" // 2 ^1
+        };
+ 
 
         static void Main(string[] args)
         {
-            Point a = new Point(10, 15);
-            a.DrawPoint();
-            a.X = 30;
-            a.SetY(20);
-            int x = a.X;
-            int y = a.GetY();
-            Console.WriteLine($"x_{x},y_{y}");
+            Point a = new Point(0,0);
+            Console.WriteLine(a[0]);
+            a[0] = "change";
+            Console.WriteLine(a[0]);
+
+
+            //Range p = 0..2;
+            //string[] list = words[p];
+            //for(int i=0; i<list.Length; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+            //Console.WriteLine(words[1]);
+            //Index k = ^1;
+            //Console.WriteLine(words[k]);
+            //Point a = new Point(10, 15);
+            //a.DrawPoint();
+            //a.X = 30;
+            //a.SetY(20);
+            //int x = a.X;
+            //int y = a.GetY();
+            //Console.WriteLine($"x_{x},y_{y}");
 
 
 

8a21c5a5c11bf634c05bee415ba36c72e1f5180a
sakurahuang <sakurahuang@tencent.com>
2023-07-03 20:08:33 +0800

    exception

---
+++ exceptionTest/exceptionTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exceptionTest
+{
+    public class Calculator
+    {
+        public int devide(int numerator, int denomenator)
+        {
+            return numerator / denomenator;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.devide(5, 0);
+            } catch (Exception)
+            {
+                Console.WriteLine("Test exception");
+            }
+        }
+    }
+}
